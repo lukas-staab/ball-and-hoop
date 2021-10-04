6af1634db906a3f332249983ddee20d2fd9a1eac
Lukas Staab <lukas.staab@tu-ilmenau.de>
2021-10-04 16:48:01 +0200

    gitignore wip chessboard.py

--- chessboard.py
+++ chessboard.py
@@ -2,13 +2,14 @@ import cv2
 import numpy as np
 import pathlib
 
+
 def calibrate_chessboard(dir_path, image_format, square_size, width, height):
-    '''Calibrate a camera using chessboard images.'''
+    """Calibrate a camera using chessboard images."""
     # termination criteria
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
     # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(8,6,0)
-    objp = np.zeros((height*width, 3), np.float32)
+    objp = np.zeros((height * width, 3), np.float32)
     objp[:, :2] = np.mgrid[0:width, 0:height].T.reshape(-1, 2)
 
     objp = objp * square_size
@@ -19,6 +20,8 @@ def calibrate_chessboard(dir_path, image_format, square_size, width, height):
 
     images = pathlib.Path(dir_path).glob(f'*.{image_format}')
     print(str(len(list(images))) + " images found.")
+    if len(list(images)) == 0:
+        return
     # Iterate through all images
     for fname in images:
         img = cv2.imread(str(fname))
