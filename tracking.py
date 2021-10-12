0631817bf96a8d358833f42c617c703dfd94ae4d
Lukas Staab <lukas.staab@tu-ilmenau.de>
2021-10-12 15:44:37 +0200

    wip

--- tracking.py
+++ tracking.py
@@ -1,6 +1,7 @@
 # import the necessary packages
 from collections import deque
 
+import imutils as imutils
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
@@ -22,7 +23,8 @@ rawCapture = PiRGBArray(camera, size=(640, 480))
 # list of tracked points
 greenLower = (29, 86, 6)
 greenUpper = (64, 255, 255)
-pts = deque(maxlen=10)
+buffer = 10
+pts = deque(maxlen=buffer)
 
 
 [camera_matrix, dist_matrix] = utils.load_coefficients('./calibration_chessboard.yml')
@@ -43,8 +45,43 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     mask = cv2.inRange(hsv, greenLower, greenUpper)
     mask = cv2.erode(mask, None, iterations=2)
     mask = cv2.dilate(mask, None, iterations=2)
+
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    # only proceed if at least one contour was found
+    center = None
+    if len(cnts) > 0:
+        # find the largest contour in the mask, then use
+        # it to compute the minimum enclosing circle and
+        # centroid
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        # only proceed if the radius meets a minimum size
+        if radius > 10:
+            # draw the circle and centroid on the frame,
+            # then update the list of tracked points
+            cv2.circle(image, (int(x), int(y)), int(radius),
+                       (0, 255, 255), 2)
+            cv2.circle(image, center, 5, (0, 0, 255), -1)
+    # update the points queue
+    pts.appendleft(center)
+
+    # loop over the set of tracked points
+    for i in range(1, len(pts)):
+        # if either of the tracked points are None, ignore
+        # them
+        if pts[i - 1] is None or pts[i] is None:
+            continue
+        # otherwise, compute the thickness of the line and
+        # draw the connecting lines
+        thickness = int(np.sqrt(buffer / float(i + 1)) * 2.5)
+        cv2.line(image, pts[i - 1], pts[i], (0, 0, 255), thickness)
+
     # show the frame
-    cv2.imshow("Frame", mask)
+    cv2.imshow("Frame", image)
     key = cv2.waitKey(1) & 0xFF
     # clear the stream in preparation for the next frame
     rawCapture.truncate(0)
