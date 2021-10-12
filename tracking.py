b7da0f21e10b4d5fd2a87d4da678e971a97a236a
Lukas Staab <lukas.staab@tu-ilmenau.de>
2021-10-12 15:22:50 +0200

    WIP Tracking

---
+++ tracking.py
@@ -0,0 +1,53 @@
+# import the necessary packages
+from collections import deque
+
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import argparse
+import cv2
+import utils
+import numpy as np
+import pathlib
+
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 90
+rawCapture = PiRGBArray(camera, size=(640, 480))
+# allow the camera to warmup
+
+# define the lower and upper boundaries of the "green"
+# ball in the HSV color space, then initialize the
+# list of tracked points
+greenLower = (29, 86, 6)
+greenUpper = (64, 255, 255)
+pts = deque(maxlen=args["buffer"])
+
+
+[camera_matrix, dist_matrix] = utils.load_coefficients('./calibration_chessboard.yml')
+
+time.sleep(0.1)
+
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    # grab the raw NumPy array representing the image, then initialize the timestamp
+    # and occupied/unoccupied text
+    image = frame.array
+    undistort = cv2.undistort(image, camera_matrix, dist_matrix)
+    blurred = cv2.GaussianBlur(undistort, (11, 11), 0)
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+    # construct a mask for the color "green", then perform
+    # a series of dilations and erosions to remove any small
+    # blobs left in the mask
+    mask = cv2.inRange(hsv, greenLower, greenUpper)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+    # show the frame
+    cv2.imshow("Frame", mask)
+    key = cv2.waitKey(1) & 0xFF
+    # clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
--- undistort.py
+++ undistort.py
@@ -6,7 +6,15 @@ import cv2
 
 img = cv2.imread("./storage/chessboard-calibration/" + input("Chessboard Number: ") + ".png")
 
-corrected_img = cv2.undistort(img, camera_matrix, dist_matrix, None, None)
+height, width = img.shape[:2]
+
+newcameramatrix, _ = cv2.getOptimalNewCameraMatrix(
+    camera_matrix, dist_matrix, (width, height), 0.5, (width, height)
+)
+
+print(img.shape[:2])
+
+corrected_img = cv2.undistort(img, camera_matrix, dist_matrix, None, newcameramatrix)
 
 cv2.imwrite("undistort.png", corrected_img)
 
