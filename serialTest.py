49357f9bb8d2c2e00559e201d721a68f77f46d91
Lukas Staab <lukas.staab@tu-ilmenau.de>
2021-11-10 18:14:19 +0100

    right port and wait time

--- serialTest.py
+++ serialTest.py
@@ -4,13 +4,13 @@ import time
 
 #port = "/dev/ttyAMA0"  # Raspberry Pi 2
 #port = "/dev/ttyS0"    # Raspberry Pi 3
-port = "/dev/ttyUSB0"
+port = "/dev/serial0"
 
 ser = serial.Serial(port, baudrate = 9600)
 print("starting")
 value = 1
 while True:
-    time.sleep(1)
+    time.sleep(.1)
     ser.write(value.to_bytes(1, byteorder='big'))
     print('Send: ' + str(value))
     value = (value +1) % 256
