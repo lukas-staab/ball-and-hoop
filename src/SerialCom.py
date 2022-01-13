94154eef001257ec73bc893260dc3c9eadcc6e50
Lukas Staab <l.staab@posteo.de>
2022-01-13 16:19:17 +0100

    serial class wrapper

---
+++ src/SerialCom.py
@@ -0,0 +1,12 @@
+import serial
+
+class SerialCom:
+    def __init__(self, port='/dev/serial0', baud=9600):
+        self.serial = serial.Serial(port, baudrate = baud)
+
+    def write(self, angle, rad):
+        angel_byte = angle.to_bytes(2, byteorder='big')
+        rad_byte = rad.to_bytes(2, byteorder='big')
+        self.serial.write(angel_byte)
+        self.serial.write(rad_byte)
+
