5acd64d356599c605363d0dff468d10c7bbfc027
Lukas Staab <l.staab@posteo.de>
2022-01-13 16:00:09 +0100

    serial ramp

--- serialTest.py
+++ serialTest.py
@@ -4,18 +4,18 @@ import time
 
 #port = "/dev/ttyAMA0"  # Raspberry Pi 2
 #port = "/dev/ttyS0"    # Raspberry Pi 3
-port = "/dev/serial0"
+port = "/dev/ttyUSB0"
 
-ser = serial.Serial(port, baudrate = 1200)
+ser = serial.Serial(port, baudrate = 9600)
 print("starting")
 value = 1
 while True:
     time.sleep(1)
-    bArray = ord("A")
-    ser.write(value)
+    ser.write(value.to_bytes(1, byteorder='big'))
     print('Send: ' + str(value))
-    value += 1
+    value = (value +1) % 256
     nbChars = ser.inWaiting()
     if nbChars > 0:
         data = ser.read(nbChars)
+        data = int(data)
         print(data)
