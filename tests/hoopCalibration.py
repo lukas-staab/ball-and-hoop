cd81d61da6fffa27e9e81b0384e1fdb00fff8be7
Lukas Staab <lukas.staab@tu-ilmenau.de>
2022-04-12 17:06:45 +0100

    working calibration pic transformation

--- tests/hoopCalibration.py
+++ tests/hoopCalibration.py
@@ -14,7 +14,7 @@ camera = PiCamera()
 camera.resolution = (640, 480)
 camera.framerate = 90
 rawCapture = PiRGBArray(camera, size=(640, 480))
-[camera_matrix, dist_matrix] = load_coefficients('../storage/chessboard-calibration/calibration_chessboard.yml')
+[camera_matrix, dist_matrix] = load_coefficients('storage/chessboard-calibration/calibration_chessboard.yml')
 
 # allow the camera to warmup
 time.sleep(0.1)
@@ -23,11 +23,11 @@ time.sleep(0.1)
 camera.capture(rawCapture, format="bgr", use_video_port=True)
 # grab the raw NumPy array representing the image, then initialize the timestamp
 # and occupied/unoccupied text
-image = rawCapture.array
-image = cv2.undistort(image, camera_matrix, dist_matrix)
+image_raw = rawCapture.array
+image_undis = cv2.undistort(image_raw, camera_matrix, dist_matrix)
 
 # convert to hsv colorspace
-hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+hsv = cv2.cvtColor(image_undis, cv2.COLOR_BGR2HSV)
 # lower bound and upper bound for Orange color
 lower_bound = np.array([10, 20, 20])
 upper_bound = np.array([30, 255, 255])
@@ -37,8 +37,13 @@ mask = cv2.inRange(hsv, lower_bound, upper_bound)
 kernel = np.ones((7, 7), np.uint8)
 mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
 mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-segmented_img = cv2.bitwise_and(image, image, mask=mask)
-filename = "storage/hoop-calibration/" + input("File name without .png: ") + ".png"
+segmented_img = cv2.bitwise_and(image_undis, image_undis, mask=mask)
+filename = input("File name without .png: ")
+pathRaw =  "storage/hoop-calibration/" + filename + "-raw.png"
+pathUndis =  "storage/hoop-calibration/" + filename + "-undistorted.png"
+path = "storage/hoop-calibration/" + filename + ".png"
 # save the frame
-cv2.imwrite(filename, segmented_img)
+cv2.imwrite(pathRaw, image_raw)
+cv2.imwrite(pathUndis, image_undis)
+cv2.imwrite(path, segmented_img)
 print("Saved")
--- tests/undistort.py
+++ tests/undistort.py
@@ -1,6 +1,7 @@
 import repackage
 repackage.up()
 from src.chessboard.utils import load_coefficients
-
-coef = load_coefficients('../storage/chessboard-calibration/calibration_chessboard.yml')
-print(coef)
+yml = open('storage/chessboard-calibration/calibration_chessboard.yml', "r").read()
+print(yml)
+[camera_matrix, dist_matrix] = load_coefficients('storage/chessboard-calibration/calibration_chessboard.yml')
+print(camera_matrix)
