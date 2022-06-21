bfc71ea024c797e57dda1854ca702080fa65967c
Lukas Staab <l.staab@posteo.de>
2022-06-21 14:09:24 +0200

    sleep for resource release

--- src/PiVideoStream.py
+++ src/PiVideoStream.py
@@ -43,6 +43,7 @@ class PiVideoStream:
             if awb == 'manuell':
                 wb = WhiteBalancing()
                 gains = wb.calculate()
+                time.sleep(1)
                 self.camera.awb_mode = 'off'
                 self.camera.awb_gains = gains
             elif type(awb) is tuple:
