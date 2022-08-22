1bdf16c59e5aa1229fb6629bafcb8c19508659ab
Lukas Staab <l.staab@posteo.de>
2022-08-23 01:52:06 +0200

    trying different codec

--- src/faker/save.py
+++ src/faker/save.py
@@ -3,14 +3,14 @@ import time
 import os
 import cv2
 
-def saveVideo(file_name, fps=60, length=1, resolution_no=2, wb_gains=None):
+def saveVideo(file_name, fps=60, length=5, resolution_no=2, wb_gains=None):
     dirName = "storage/faker/"
     file_path = dirName + file_name + '.avi'
 
     if not os.path.exists(dirName):
         os.mkdir(dirName)
 
-    video_writer = cv2.VideoWriter(file_path, cv2.VideoWriter_fourcc('I', '4', '2', '0'),
+    video_writer = cv2.VideoWriter(file_path, cv2.VideoWriter_fourcc(* 'RGBA'),
                                    fps, VideoStream.resolutions[resolution_no])
     vid = VideoStream(resolution_no=resolution_no, framerate=fps, wb_gains=wb_gains, as_hsv=False)
     sec = time.time()
