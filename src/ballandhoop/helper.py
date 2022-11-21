2553d6f64e8395ea8cd671357d14bc51c2a6cd23
Lukas Staab <l.staab@posteo.de>
2022-11-21 18:24:59 +0100

    fixed wrong color coding

--- src/ballandhoop/helper.py
+++ src/ballandhoop/helper.py
@@ -7,7 +7,7 @@ import cv2
 def get_bgr_picture(faker_path:str = None):
     """ If faker_path is given, return the picture in path, otherwise take a picture from pi cam """
     if faker_path is not None:
-        print("WARNING: Taking a virtual picture from file: " + faker_path)
+        print("WARNING: Taking a virtual picture from file, not from cam: " + faker_path)
         return cv2.imread(faker_path)
     else:
         # no fake picture given. expecting to be on a raspberry pi
@@ -21,7 +21,7 @@ def get_bgr_picture(faker_path:str = None):
 
 def get_hsv_picture(faker_path:str = None):
     """ See @get_bgr_picture - but transformed to HLS Colors """
-    return cv2.cvtColor(get_bgr_picture(faker_path), cv2.COLOR_BGR2HLS)
+    return cv2.cvtColor(get_bgr_picture(faker_path), cv2.COLOR_BGR2HSV)
 
 def reset_content_of_dir(dir_name : str):
     if os.path.exists(dir_name):
